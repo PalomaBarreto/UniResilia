f8b56d0ae3a574fa1dcf36e1aee3a174772473a1
Paloma Barreto de Carvalho <86432423+PalomaBarreto@users.noreply.github.com>
2022-01-06 00:21:37 -0300

    adicionando os componentes Button e Footer

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from 'styled-components'
+
+function Button()
+{
+  return
+  (
+    <Button>
+
+    </Button>
+  )
+}
+
+export default Button
+
+const Button = styled.button`
+
+`
