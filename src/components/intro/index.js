74a547ffe756e67976859b7c55e50465da1b6192
nogueiraravel <ravel.santos@hotmail.com.br>
2022-01-07 11:19:43 -0300

    organização e teste da intro

---
+++ src/components/intro/index.js
@@ -0,0 +1,3 @@
+import Intro from "./intro.jsx";
+
+export default Intro
---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,12 @@
+import "./style.css"
+import React, { Component } from "react";
+
+export default class Intro extends Component{
+    render(){
+        return(
+            <div className="Principal">
+                FÉ
+            </div>
+        )
+    }
+}
