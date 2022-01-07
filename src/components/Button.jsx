fcdaa168cc578c1fab9c34555c12c015a8b89ee3
Paloma Barreto de Carvalho <86432423+PalomaBarreto@users.noreply.github.com>
2022-01-07 19:33:03 -0300

    componente button funcionando

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,30 @@
-import React from "react"
+import React from 'react'
 import styled from 'styled-components'
 
-function Button()
-{
-  return
-  (
-    <Button>
 
-    </Button>
+function Button(props)
+{
+  return(
+    <Btn onClick={props.click}>
+    {props.children}
+    </Btn>
   )
 }
-
-export default Button
-
-const Button = styled.button`
-
+  
+  
+const Btn = styled.button`
+  background-color: var(--orange);
+  color: var(--navy);
+  font-size: 18px;
+  height: 40px;
+  padding: 8px 10px;
+  border-radius: 5px;
+  border: none;
+  transition: all .3s;
+  &:hover {
+    color: var(--yellow);
+    
+}
 `
+  
+export default Button
