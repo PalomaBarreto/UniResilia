ab718ea3cdff505c12fd261357a0fe0efb634700
nogueiraravel <ravel.santos@hotmail.com.br>
2022-01-10 14:51:26 -0300

    criacao dos depoimentos e estilização inicial home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Intro from "../components/Intro";
 import CardHome from "../components/CardHome";
+import Depoimentos from "../components/Depoimentos";
 
 export default function Home() {
   return (
     <div>
       <Intro
-        description={"Alguma baboseira"}
+        description={"Transforme oportunidades em possibilidades."}
         title={"A melhor universidade Tech do Brasil!"}
       />
+      <Depoimentos/>
       <CardHome />
     </div>
   );
