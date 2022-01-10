d212baf8599039c4ec42afd40d673310fd945fc2
nogueiraravel <ravel.santos@hotmail.com.br>
2022-01-10 15:17:47 -0300

    criacao das modalidade

---
+++ src/components/Modalidade.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Modalidade() {
+  return (
+    <div>
+      <DivCards>
+        <Titulo>O melhor para você. E do seu jeito</Titulo>
+        <Paragrafo>
+          Precisa de flexibilidade de horários? Aqui, você encontra a modalidade
+          que mais combina com a sua rotina.
+        </Paragrafo>
+      </DivCards>
+    </div>
+  );
+}
+const DivCards = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+
+  margin-bottom: 15px;
+`;
+
+const Titulo = styled.h1`
+  margin: 70px 0px 20px 0px;
+  font-size: 50px;
+  color: #001b44;
+  text-shadow: 1px 1px 4px var(--navy);
+`;
+const Paragrafo = styled.p`
+  font-size: 25px;
+  color: #001b44;
+`;
