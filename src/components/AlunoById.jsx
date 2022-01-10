7b35315e3233885ed6f55413b83f76844419d8bd
renanmartins1813 <renanmartins1813@github.com>
2022-01-10 17:29:36 -0500

    commitando agora pra não chorar depois

--- src/components/AlunoById.jsx
+++ src/components/AlunoById.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { getStudentById } from '../utils/Api';
+import Aluno from './Aluno';
+import Loading from './Loading';
 
 export default function AlunoById() {
-    return (
-        <div>
-            <p>AlunoByID</p>
-        </div>
-    )
+    const [id, setId] = useState(0);
+    const [aluno, setAluno] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(()=>{
+        getStudentById(2).then((res)=>{
+            setAluno(res);
+            setIsLoading(false);
+            console.log(aluno)
+        });
+
+    },[isLoading])
+    
+    if(isLoading){
+        return(
+            <Loading />
+        )
+    }
+    else{
+        return(
+            <p>Aluno</p>
+        )
+    }
 }
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,9 +1,26 @@
 const url = 'https://afternoon-thicket-89765.herokuapp.com/students';
 
-export async function getStudents(){
+export async function getStudents() {
     const data = await fetch(
-        url,
-        {
+        url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+    ).then(
+        res => {
+            return res.json();
+        }
+    )
+
+    return data.students;
+}
+
+export async function getStudentById(id) {
+    const data = await fetch(
+        `${url}/${id}`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -16,5 +33,5 @@ export async function getStudents(){
         }
     )
 
-    return data.students;
+    return data
 }
