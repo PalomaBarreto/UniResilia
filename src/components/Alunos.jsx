24f8b7430cc3bd46ab2861ae2784b0226c901043
renanmartins1813 <renanmartins1813@github.com>
2022-01-09 19:31:36 -0500

    Loading component

--- src/components/Alunos.jsx
+++ src/components/Alunos.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { getStudents } from '../utils/Api';
+import Loading from './Loading';
 
 export default function Alunos() {
-    return (
-        <div>
-            Alunos
-        </div>
-    )
+    const [alunos, setAlunos] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    // useEffect(()=>{
+    //     const students = getStudents();
+    //     setAlunos(students);
+    //     setIsLoading(false);
+    // },[isLoading])
+    
+    if(isLoading){
+        return(
+            <Loading/>
+        );
+    }
+    else{
+        return (
+            <div>Tristeza</div>
+        )
+    }
 }
