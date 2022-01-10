c8b27e654ec1dff4ae9b4654748dfbb33ff34672
renanmartins1813 <renanmartins1813@github.com>
2022-01-10 15:42:32 -0500

    Alunos ok?

--- src/components/Aluno.jsx
+++ src/components/Aluno.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import styled from 'styled-components';
 
-export default function Aluno({aluno:{ID, NAME, nascimento, carreira, cpf, email, registro}}) {
-    console.log(ID)
+const LiCard = styled.li`
+    width: 350px;
+    height: 170px;
+    background-color: aliceblue;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: space-evenly;
+    align-items: center;
+    border-radius: 0 30px 0 30px;
+`;
+
+export default function Aluno({ aluno: { ID, NAME, BIRTHDATE, CPF, EMAIL, CAREER, REGISTRATIONDATE } }) {
+
     return (
-        <p>{ID}</p>
+        <LiCard key={ID}>
+            <p>{NAME}</p>
+            <p>CPF: {CPF}.</p>
+            <p>Data de nascimento: {BIRTHDATE}.</p>
+            <p>Email: {EMAIL}.</p>
+            <p>Admissão: {REGISTRATIONDATE}.</p>
+            <p>Carreira: {CAREER}.</p>
+        </LiCard>
     )
 }
--- src/components/Alunos.jsx
+++ src/components/Alunos.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { getStudents } from '../utils/Api';
 import Aluno from './Aluno';
 import Loading from './Loading';
+
+const UlContainer = styled.ul`
+    width: 1000px;
+    min-height: 700px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: center;
+`;
 
 export default function Alunos() {
     const [alunos, setAlunos] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        getStudents().then((res)=>{
+        getStudents().then((res) => {
             setAlunos(res);
             setIsLoading(false);
         }
@@ -23,9 +33,9 @@ export default function Alunos() {
     }
     else {
         return (
-            <ul>
-                {alunos.map((aluno, index)=>{
-                    return(
+            <UlContainer>
+                {alunos.map((aluno, index) => {
+                    return (
                         <Aluno
                             // id={aluno.ID}
                             // nome={aluno.NAME}
@@ -38,7 +48,18 @@ export default function Alunos() {
                         />
                     );
                 })}
-            </ul>
+            </UlContainer>
         )
     }
 }
+
+/* <Aluno
+    id={aluno.ID}
+    nome={aluno.NAME}
+    nascimento={aluno.BIRTHDATE}
+    carreira={aluno.CAREER}
+    cpf={aluno.CPF}
+    email={aluno.EMAIL}
+    registro={aluno.REGISTRATIONDATE}
+    aluno={aluno}
+/> */
