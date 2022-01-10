746e456643e876d63a1d7dc26ca481a5b9b17376
renanmartins1813 <renanmartins1813@github.com>
2022-01-10 17:46:17 -0500

    AlunoById basico ok

--- src/components/AlunoById.jsx
+++ src/components/AlunoById.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
@@ -8,36 +7,35 @@ import Loading from './Loading';
 
 export default function AlunoById() {
     const [id, setId] = useState(0);
-    const [aluno, setAluno] = useState({});
+    const [aluno, setAluno] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>{
-        getStudentById(2).then((res)=>{
-            setAluno(res);
+    useEffect(() => {
+        getStudentById(1).then((res) => {
+            console.log(res)
+            console.log('res acima')
+            setAluno(res.req);
             setIsLoading(false);
             console.log(aluno)
+            console.log('aluno a cima')
         });
 
-    },[isLoading])
-    
-    if(isLoading){
-        return(
+    }, [isLoading])
+
+    if (isLoading) {
+        return (
             <Loading />
         )
     }
-    else{
-        return(
-            <p>Aluno</p>
+    else {
+        return (
+            <div>
+                {aluno.map(aluno => {
+                    return (
+                        <Aluno aluno={aluno} />
+                    )
+                })}
+            </div>
         )
     }
-=======
-import React from 'react'
-
-export default function AlunoById() {
-    return (
-        <div>
-            <p>AlunoByID</p>
-        </div>
-    )
->>>>>>> 24f8b7430cc3bd46ab2861ae2784b0226c901043
 }
